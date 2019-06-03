f60065f7ecbcc864b12744f62103ddd3a0b7a4f2
Jeremie <jeremie.chatillon@gmail.com>
2019-06-03 19:18:41 +0200

    boostsrap maggle

---
+++ Backend/app/controllers/LoginController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+@Singleton
+class LoginController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def login = Action {
+    Ok(views.html.login())
+  }
+
+}
